852bfe43d6cc139d7375757b7e6bd7e7af409c3e
Benjamin Otte <otte@redhat.com>
2025-03-22 16:23:37 +0100

    gpu: Add GSK_GPU_IMAGE_DOWNLOADABLE flag
    A downloadable image is an image whose data can be copied unmodified
    into CPU memory.
    
    On Vulkan, this is decided by the driver supporting the
    VK_FORMAT_FEATURE_TRANSFER_SRC_BIT for the given image/format.
    On GL, everything uploaded with glTexImage() can be downloaded again,
    but EGL image data from dmabufs cannot.

--- gsk/gpu/gskglimage.c
+++ gsk/gpu/gskglimage.c
@@ -178,6 +178,7 @@ gsk_gl_image_new (GskGLDevice      *device,
     }
   if (swizzle_is_identity (swizzle))
     flags |= GSK_GPU_IMAGE_BLIT;
+  flags |= GSK_GPU_IMAGE_DOWNLOADABLE;
 
   gsk_gpu_image_setup (GSK_GPU_IMAGE (self),
                        flags,
@@ -249,8 +250,12 @@ gsk_gl_image_new_for_texture (GskGLDevice      *device,
   else
     {
       flags &= ~(GSK_GPU_IMAGE_CAN_MIPMAP | GSK_GPU_IMAGE_MIPMAP);
-      if (swizzle_is_identity (swizzle) && (extra_flags & GSK_GPU_IMAGE_EXTERNAL) == 0)
-        flags |= GSK_GPU_IMAGE_BLIT;
+      if (!(extra_flags & GSK_GPU_IMAGE_EXTERNAL))
+        {
+          if (swizzle_is_identity (swizzle))
+            flags |= GSK_GPU_IMAGE_BLIT;
+          flags |= GSK_GPU_IMAGE_DOWNLOADABLE;
+        }
     }
   
   gsk_gpu_image_setup (GSK_GPU_IMAGE (self),
