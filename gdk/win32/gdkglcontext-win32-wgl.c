0b7e4dc12e26b0d075cff9513bd734d127d4c62e
Chun-wei Fan <fanchunwei@src.gnome.org>
2025-03-26 11:06:45 +0800

    gdkglcontext-win32-wgl.c: Apply scale for glAddSwapHintRectWIN()
    We still need to pass the scaled positions and size to
    glAddSwapHintRectWIN(), as we do when we call Windows API functions to
    position or resize surfaces.
    
    This reverts the changes on gdkwin32glcontext-win32-wgl.c in MR !8244.
    
    Fixes items not rendering or partially rendering when the GL renderer is
    being used.

--- gdk/win32/gdkglcontext-win32-wgl.c
+++ gdk/win32/gdkglcontext-win32-wgl.c
@@ -114,6 +114,7 @@ gdk_win32_gl_context_wgl_end_frame (GdkDrawContext *draw_context,
       context->old_updated_area[0])
     {
       int num_rectangles = cairo_region_num_rectangles (context->old_updated_area[0]);
+      int scale = surface_win32->surface_scale;
       cairo_rectangle_int_t rectangle;
 
       for (int i = 0; i < num_rectangles; i++)
@@ -126,10 +127,10 @@ gdk_win32_gl_context_wgl_end_frame (GdkDrawContext *draw_context,
            */
           rectangle.y = surface->height - rectangle.y - rectangle.height;
 
-          context_wgl->ptr_glAddSwapHintRectWIN (rectangle.x,
-                                                 rectangle.y,
-                                                 rectangle.width,
-                                                 rectangle.height);
+          context_wgl->ptr_glAddSwapHintRectWIN (rectangle.x * scale,
+                                                 rectangle.y * scale,
+                                                 rectangle.width * scale,
+                                                 rectangle.height * scale);
         }
     }
 
