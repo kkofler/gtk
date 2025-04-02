d8152812d11fd5962d66b0ccdc0824d926a45544
Kevin Kofler <kevin.kofler@chello.at>
2025-04-03 00:18:56 +0200

    Revert "gl: Require support for GLsync"
    This reverts commit 2274bca95b4eb21276ffb83fd50ceaee9a2991b8.

--- gdk/gdkglcontextprivate.h
+++ gdk/gdkglcontextprivate.h
@@ -36,6 +36,7 @@ typedef enum {
   GDK_GL_FEATURE_EXTERNAL_OBJECTS_WIN32     = 1 << 4,
   GDK_GL_FEATURE_UNPACK_SUBIMAGE            = 1 << 5,
   GDK_GL_FEATURE_VERTEX_HALF_FLOAT          = 1 << 6,
+  GDK_GL_FEATURE_SYNC                       = 1 << 7,
 } GdkGLFeatures;
 
 #define GDK_GL_N_FEATURES 3
