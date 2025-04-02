7d842a5738c5337b51051bebb4cea4131594340b
Kevin Kofler <kevin.kofler@chello.at>
2025-04-03 00:14:18 +0200

    Revert "gl: Remove unpack-subimage checks"
    This reverts commit 7458461d061cebf0e5f49440858dc36d182f7da2.

--- gdk/gdkglcontextprivate.h
+++ gdk/gdkglcontextprivate.h
@@ -32,6 +32,7 @@ typedef enum {
   GDK_GL_FEATURE_DEBUG                      = 1 << 0,
   GDK_GL_FEATURE_BASE_INSTANCE              = 1 << 1,
   GDK_GL_FEATURE_BUFFER_STORAGE             = 1 << 2,
+  GDK_GL_FEATURE_UNPACK_SUBIMAGE            = 1 << 5,
 } GdkGLFeatures;
 
 #define GDK_GL_N_FEATURES 3
